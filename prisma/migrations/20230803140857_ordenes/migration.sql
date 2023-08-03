83c88a5169014aee2c02ae8cc6b26f8fd1933894
Martin Brisset <brissetmartin@gmail.com>
2023-08-03 11:22:23 -0300

    init

---
+++ prisma/migrations/20230803140857_ordenes/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `Orden` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nombre` VARCHAR(191) NOT NULL,
+    `fecha` VARCHAR(191) NOT NULL,
+    `total` DOUBLE NOT NULL,
+    `pedido` JSON NOT NULL,
+    `estado` INTEGER NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
